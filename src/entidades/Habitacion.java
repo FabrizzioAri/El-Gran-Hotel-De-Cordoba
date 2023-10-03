af6f829bba0f0c1dca47ef866bb1ec83b8a23b6a
FabrizzioAri <fabriariatello@gmail.com>
2023-10-03 20:04:32 -0300

    V1
    se crea el paquete entidades

---
+++ src/entidades/Habitacion.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Fabrizzio
+ */
+public class Habitacion {
+    private int idHabitacion;
+    private int idTipo;
+    private boolean estado;
+    private int piso;
+
+    public Habitacion() {
+    }
+
+    public Habitacion(int idHabitacion, int idTipo, int piso) {
+        this.idHabitacion = idHabitacion;
+        this.idTipo = idTipo;
+        this.piso = piso;
+    }
+
+    public Habitacion(int idHabitacion, int idTipo, boolean estado, int piso) {
+        this.idHabitacion = idHabitacion;
+        this.idTipo = idTipo;
+        this.estado = estado;
+        this.piso = piso;
+    }
+
+    public int getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public void setIdHabitacion(int idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
+
+    public int getIdTipo() {
+        return idTipo;
+    }
+
+    public void setIdTipo(int idTipo) {
+        this.idTipo = idTipo;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public int getPiso() {
+        return piso;
+    }
+
+    public void setPiso(int piso) {
+        this.piso = piso;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitacion{" + "idHabitacion=" + idHabitacion + ", IdTipo=" + idTipo + ", Estado=" + estado + ", Piso=" + piso + '}';
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ src/entidades/Reserva.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Fabrizzio
+ */
+public class Reserva {
+    private int idReserva;
+    private Huesped huesped;
+    private Habitacion habitacion;
+    private LocalDate fechaEntrada;
+    private LocalDate fechaSalida;
+    private int cantPersonas;
+    private int precio;
+    private boolean estado;
+    
+    public Reserva() {
+    }
+
+    public Reserva(Huesped huesped, Habitacion habitacion, LocalDate fechaEntrada, LocalDate fechaSalida, int cantPersonas, int precio, boolean estado) {
+        this.huesped = huesped;
+        this.habitacion = habitacion;
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+        this.cantPersonas = cantPersonas;
+        this.precio = precio;
+        this.estado = estado;
+    }
+
+    public Reserva(int idReserva, Huesped huesped, Habitacion habitacion, LocalDate fechaEntrada, LocalDate fechaSalida, int cantPersonas, int precio, boolean estado) {
+        this.idReserva = idReserva;
+        this.huesped = huesped;
+        this.habitacion = habitacion;
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+        this.cantPersonas = cantPersonas;
+        this.precio = precio;
+        this.estado = estado;
+    }
+
+    public int getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(int idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public Huesped getHuesped() {
+        return huesped;
+    }
+
+    public void setHuesped(Huesped huesped) {
+        this.huesped = huesped;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public LocalDate getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(LocalDate fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public LocalDate getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(LocalDate fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public int getCantPersonas() {
+        return cantPersonas;
+    }
+
+    public void setCantPersonas(int cantPersonas) {
+        this.cantPersonas = cantPersonas;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" + "idReserva=" + idReserva + ", Huesped=" + huesped + ", Habitacion=" + habitacion + ", Fecha Entrada=" + fechaEntrada + ", Fecha Salida=" + fechaSalida + ", Cantidad de Personas=" + cantPersonas + ", Precio=" + precio + ", Estado=" + estado + '}';
+    }
+    
+    
+}
