2fa4cc0b25178db40500c7a82e27e54e0ac4d73e
FabrizzioAri <fabriariatello@gmail.com>
2023-10-09 19:47:49 -0300

    v3 Completado HuespedData

--- src/el/gran/hotel/de/cordoba/ElGranHotelDeCordoba.java
+++ src/el/gran/hotel/de/cordoba/ElGranHotelDeCordoba.java
@@ -19,13 +19,15 @@ public class ElGranHotelDeCordoba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       Huesped hue1 = new Huesped("Carlos","Montiel",45794166,"elpity@gmail.com",351479865,true);
-       Huesped hue2 = new Huesped("Octavio","Calles",45845666,"octavio@gmail.com",355566135,true);
-       Huesped hue3 = new Huesped("Jazmin","Saavedra",41215448,"jaz@gmail.com",116465484,true);
-        Huesped hue4 = new Huesped("prueba","S",41254548,"prueba@gmail.com",113235484,true);
+       //Huesped hue1 = new Huesped("Carlos","Montiel",45794166,"elpity@gmail.com",351479865,true);
+       //Huesped hue2 = new Huesped("Octavio","Calles",45845666,"octavio@gmail.com",355566135,true);
+       //Huesped hue3 = new Huesped("Jazmin","Saavedra",41215448,"jaz@gmail.com",116465484,true);
+       Huesped hue4 = new Huesped(5,"prueba","Sas",41254548,"prueba@gmail.com",113235484,true);
+       Huesped Hued4 = hue4;
        HuespedData huespedData = new HuespedData();
-       huespedData.agregarHuesped(hue4);
-       
+       //huespedData.modificarHuesped(Hued4);
+       huespedData.eliminarHuesped(5);
+     
         
     }
     
